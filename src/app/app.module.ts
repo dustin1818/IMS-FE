5f0065e2943dfe35b1393d272069920dcd7f2889
dustin1818 <dustinamoda18@gmail.com>
2021-09-30 02:18:00 +0800

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainRoutingModule } from './main/main-routing.module';
+import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/navbar/dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -22,7 +24,9 @@ import { ProfileComponent } from './components/profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainRoutingModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
