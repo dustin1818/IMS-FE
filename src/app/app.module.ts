3cc3101efca1a398be033431cf4e1ecfeca05778
dustin1818 <dustinamoda18@gmail.com>
2021-09-30 01:26:42 +0800

    adding components in component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/navbar/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    SigninComponent,
+    SignupComponent,
+    SidebarComponent,
+    FooterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
