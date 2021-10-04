3b1b8e2e339e0e2a47111070dae587b48b9d32d0
dustin1818 <dustinamoda18@gmail.com>
2021-10-05 03:59:19 +0800

    Removing unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,23 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { CommonModule } from '@angular/common';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { SupplierComponent } from './components/supplier/supplier.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+
 
 @NgModule({
   declarations: [
@@ -19,14 +33,26 @@ import { ProfileComponent } from './components/profile/profile.component';
     DashboardComponent,
     SigninComponent,
     SignupComponent,
-    SidebarComponent,
     FooterComponent,
-    ProfileComponent
+    ProfileComponent,
+    EmployeeComponent,
+    SupplierComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
     MainRoutingModule,
-    MainModule
+    MainModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,
+    MatSortModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
