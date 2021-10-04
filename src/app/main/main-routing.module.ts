d541d424ae86563c86f70ba0adc890f75eeed2f3
dustin1818 <dustinamoda18@gmail.com>
2021-10-05 04:01:28 +0800

    adding more routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from '../app.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { EmployeeComponent } from '../components/employee/employee.component';
+import { InventoryComponent } from '../components/inventory/inventory.component';
 import { ProfileComponent } from '../components/profile/profile.component';
 import { SigninComponent } from '../components/signin/signin.component';
 import { SignupComponent } from '../components/signup/signup.component';
+import { SupplierComponent } from '../components/supplier/supplier.component';
 
 
 const routes: Routes = [
+
   {path: 'signin', component:SigninComponent},
   {path: 'signup', component:SignupComponent},
   {path: 'profile', component:ProfileComponent},
+  {path: 'inventory', component:InventoryComponent},
+  {path: 'employee', component:EmployeeComponent},
+  {path: 'supplier', component:SupplierComponent},
   {path: '**', component:DashboardComponent},
+ 
 ];
 
 @NgModule({
