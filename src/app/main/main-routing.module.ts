bff3a878295e394b963de1b6730db524f76324d2
dustin1818 <dustinamoda18@gmail.com>
2021-10-20 03:16:45 +0800

    adding main route & lazy loading

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,25 +1,81 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../components/dashboard/dashboard.component';
-import { EmployeeComponent } from '../components/employee/employee.component';
-import { InventoryComponent } from '../components/inventory/inventory.component';
-import { ProfileComponent } from '../components/profile/profile.component';
 import { SigninComponent } from '../components/signin/signin.component';
 import { SignupComponent } from '../components/signup/signup.component';
-import { SupplierComponent } from '../components/supplier/supplier.component';
 
 
+// lazy loading routes
 const routes: Routes = [
+  {
+    path: 'signin',
+    component:SigninComponent
+  },
+  {
+    path: 'signup',
+    component:SignupComponent
+  },
+  {
+    path: 'employee',
+    loadChildren: () =>     import('../components/employee/routing-emp/routing-emp-routing.module')
+      .then(m => m.RoutingEmpRoutingModule)
+  },
+  {
+    path:'inventory',
+    loadChildren: () =>     import('../../app/components/inventory/routing-inv/routing-inv-routing.module')
+    .then(m => m.RoutingInvRoutingModule)
+  },
+  {
+    path:'profile',
+    loadChildren: () =>     import('../../app/components/profile/routing-prof/routing-prof-routing.module')
+    .then(m => m.RoutingProfRoutingModule)
+  },
+  {
+    path:'supplier-list',
+    loadChildren: () =>     import('../../app/components/supplier/routing-supplier/routing-supplier-routing.module')
+    .then(m => m.RoutingSupplierRoutingModule)
+  },
+  {
+    path:'supplier-modal',
+    loadChildren: () =>     import('../../app/components/supplier-modal/routing-supplier-modal/routing-supplier-modal-routing.module')
+    .then(m => m.RoutingSupplierModalRoutingModule)
+  },
 
-  {path: 'signin', component:SigninComponent},
-  {path: 'signup', component:SignupComponent},
-  {path: 'profile', component:ProfileComponent},
-  {path: 'inventory', component:InventoryComponent},
-  {path: 'employee', component:EmployeeComponent},
-  {path: 'supplier', component:SupplierComponent},
-  {path: '**', component:DashboardComponent},
- 
+  { 
+    path: 'supplier-modal/:id', 
+    loadChildren: () =>     import('../../app/components/supplier-modal/routing-supplier-modal/routing-supplier-modal-routing.module')
+    .then(m => m.RoutingSupplierModalRoutingModule)
+  },
+  { 
+    path: 'dashboard', 
+    loadChildren: () =>     import('../../app/components/dashboard/routing-dashboard/routing-dashboard-routing.module')
+    .then(m => m.RoutingDashboardRoutingModule)
+  },
+  { 
+    path: 'help', 
+    loadChildren: () =>     import('../../app/components/help/routing-help/routing-help-routing.module')
+    .then(m => m.RoutingHelpRoutingModule)
+  },
+  { 
+    path: 'about', 
+    loadChildren: () =>     import('../../app/components/about/routing-about/routing-about-routing.module')
+    .then(m => m.RoutingAboutRoutingModule)
+  },
 ];
+
+
+
+// const routes: Routes = [
+
+//   { path: 'signin', component: SigninComponent },
+//   { path: 'signup', component: SignupComponent },
+//   { path: 'profile', component: ProfileComponent },
+//   { path: 'inventory', component: InventoryComponent },
+//   { path: 'employee', component: EmployeeComponent },
+//   { path: 'supplier-list', component: SupplierComponent },
+//   { path: 'supplier-modal', component: SupplierModal },
+//   { path: 'editar-producto/:id', component: SupplierModal },
+//   { path: '**', component: DashboardComponent, pathMatch: 'full' }
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
