e8e87cdd83aa63802213c77c93d705283b0d875d
dustin1818 <dustinamoda18@gmail.com>
2021-10-20 03:16:11 +0800

    supplier-modal component

---
+++ src/app/components/supplier-modal/supplier-modal.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Supplier } from 'src/app/models/supplier';
+import { SupplierService } from 'src/app/services/supplier.service';
+
+@Component({
+  selector: 'app-supplier-modal',
+  templateUrl: './supplier-modal.component.html',
+  styleUrls: ['./supplier-modal.component.css']
+})
+export class SupplierModal implements OnInit {
+
+  //declaring variables
+  supplierForm: FormGroup;
+  title = 'Create Supplier';
+  id: string | null;
+
+
+  //constructor IMPORTS
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private toastr: ToastrService,
+              private _supplierService: SupplierService,
+              private aRouter: ActivatedRoute) { 
+
+        //Fields from FORMCONTROL()
+        this.supplierForm = this.fb.group({
+          supplier_no: ['', Validators.required],
+          name: ['', Validators.required],
+          phone_no: ['', Validators.required],
+          location: ['', Validators.required],
+        })
+        this.id = this.aRouter.snapshot.paramMap.get('id');
+      }
+
+
+  
+    ngOnInit(): void {
+
+      //editSupplier
+      this.editSupplier();
+    }
+
+
+  //add Supplier
+  addSupplier() {
+    //Producto
+    const SUPPLIER: Supplier = {
+      supplier_no: this.supplierForm.get('supplier_no')?.value,
+      name: this.supplierForm.get('name')?.value,
+      phone_no: this.supplierForm.get('phone_no')?.value,
+      location: this.supplierForm.get('location')?.value,
+    }
+
+    //edit supplier
+    if(this.id !== null){
+      this._supplierService.editSupplier(this.id, SUPPLIER).subscribe(data => {
+        this.toastr.info('Supplier successfully edited !', 'Supplier Registered!');
+        this.router.navigate(['/supplier-list']);
+      })
+    }else{
+
+      //add supplier
+      console.log(SUPPLIER);
+      this._supplierService.addSupplier(SUPPLIER).subscribe(data => {
+        this.toastr.success('Supplier added succesfully!', 'Supplier Added!');
+        this.router.navigate(['/supplier-list']);
+      }, error => {
+        console.log(error);
+        this.supplierForm.reset();
+      })
+    }
+
+  
+  }
+
+  //edit Supplier function
+  editSupplier() {
+    if(this.id !== null) {
+      this.title = 'Edit Supplier';
+      this._supplierService.ugradeSupplier(this.id).subscribe(data => {
+        this.supplierForm.setValue({
+          supplier_no: data.supplier_no,
+          name: data.name,
+          phone_no: data.phone_no,
+          location: data.location,
+        })
+      })
+    }
+  }
+
+}
