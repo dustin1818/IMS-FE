1d1d5ebf77453246b1ef8bc1aed0b93fb51908e4
dustin1818 <dustinamoda18@gmail.com>
2021-10-20 03:17:06 +0800

    adding models- supplier

---
+++ src/app/models/supplier.ts
@@ -0,0 +1,14 @@
+export class Supplier {
+    _id?: number;
+    supplier_no: number;
+    name: string;
+    phone_no: number;
+    location: string;
+
+    constructor(supplier_no: number, name: string, phone_no: number, location: string ){
+        this.supplier_no = supplier_no;
+        this.name = name;
+        this.phone_no = phone_no;
+        this.location = location;
+    }
+}
