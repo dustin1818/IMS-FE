75ed942542665c70ae29903873c47c28909b4cbe
dustin1818 <dustinamoda18@gmail.com>
2021-10-26 12:19:06 +0800

    registration component fix

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { AuthGuard } from "src/app/guard/auth.guard";
+
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  submitting = false;
+  form: FormGroup;
+  message:any;
+  messageClass:any;
+  previousUrl:any;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private authGuard: AuthGuard
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
+  onLoginSubmit() {
+    this.submitting = true;
+    const user = {
+      email: this.form.get('email')?.value,
+      password: this.form.get('password')?.value,
+    }
+
+    this.authService.loginUser(user).subscribe(data => {
+      if (!data['success']) {
+        this.messageClass = 'alert alert-danger'; 
+        this.message = data['message']
+        this.submitting = false; // Re-enable submit button
+      } else {
+        this.messageClass = 'alert alert-success'
+        this.message = data['message']
+        this.authService.storeUserData(data['token'], data['user'])
+        if (this.previousUrl) {
+          this.router.navigate([this.previousUrl])
+        } else {
+          this.authService.redirectToHome()
+        }
+      }
+    });
+  }
+ 
+  ngOnInit(): void {
+    if (this.authGuard.redirectUrl) {
+      this.messageClass = 'alert alert-danger'
+      this.message = "You must be logged in to view that page."
+      this.previousUrl = this.authGuard.redirectUrl
+      this.authGuard.redirectUrl = undefined
+    }
+  }
+ 
 }
+ 
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+  registerForm: FormGroup; 
+  submitting: boolean = false;
+  message:any;
+  messageClass:any;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router : Router
+  ) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        this.validateEmail
+      ])],
+      username: ['', Validators.required],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        this.validatePass
+      ])],
+      phoneNo: ['', Validators.required], // number
+      location: ['', Validators.required],
+      position: ['', Validators.required],
+    })
   }
 
-}
+  validateEmail(controls:any) {
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(controls.value)) return null;
+    return { 'validateEmail': true };
+  }
+
+  validatePass(controls:any) {
+    const regExp = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/);
+    if (regExp.test(controls.value)) return null;
+    return { 'validatePass': true };
+  }
+
+  onRegisterSubmit() {
+    this.submitting = true;
+    const user = {
+      username: this.registerForm.get('username')?.value,
+      password: this.registerForm.get('password')?.value,
+      phoneNo: this.registerForm.get('phoneNo')?.value,
+      location: this.registerForm.get('location')?.value,
+      email: this.registerForm.get('email')?.value,
+      position: this.registerForm.get('position')?.value
+    }
+    this.authService.registerUser(user).subscribe((data: Object) => {
+      if (!data['success']) {
+       this.messageClass = 'alert alert-danger'; 
+       this.message = data['message'];
+       this.submitting = false; // Re-enable submit button
+     } else {
+      this.message = data['message']; 
+       this.messageClass = 'alert alert-success'; 
+       setTimeout(() => {
+         this.router.navigate(['/signin']); // Redirect to login view
+       }, 2000);
+     }
+   })
+ }
+
+  }
+
+
