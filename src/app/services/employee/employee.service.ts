dff72c664d7cc4cae3a20be9ae71aea848879951
dustin1818 <dustinamoda18@gmail.com>
2021-11-10 19:09:52 +0800

    adding employee service

---
+++ src/app/services/employee/employee.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from 'src/app/models/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  //api from Backend can be seen in IMS/BE - app.js
+  url = 'http://localhost:3000/employees/';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployee(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  deleteEmployee(id: string): Observable<any> {
+    return this.http.delete(this.url + id);
+  }
+
+  addEmployee(employee: Employee): Observable<any> {
+    return this.http.post(this.url, employee);
+  }
+
+  ugradeEmployee(id: string): Observable<any> {
+    return this.http.get(this.url + id);
+  }
+
+  editEmployee(id: string, employee: Employee): Observable<any> {
+    return this.http.put(this.url + id, employee);
+  }
+}
